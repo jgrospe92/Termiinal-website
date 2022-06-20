708f83f39245dcbbad69ee7ecbf0bb8033534334
Jeffrey <77903214+jgrospe92@users.noreply.github.com>
2022-06-19 20:34:44 -0400

    Initialize the project
    Created Index file,
    added css, caret.js, commands.js, and main.js

---
+++ root/js/commands.js
@@ -0,0 +1,33 @@
+var today = new Date();
+const dayOfTheWeek = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+
+var twitter = "https://twitter.com/jeffrey_grospe";
+var password = dayOfTheWeek[today.getDay()];
+var linkedin = "https://www.linkedin.com/in/jeffreygrospe/";
+var github = "https://github.com/jgrospe92";
+var instagram = "https://www.instagram.com/jeffrey_grospe/";
+var email = "mailto:jeffreygrospe@yahoo.com";
+
+whois = [
+    "<br>",
+    "Hi welcome to website, My name is Jeffrey!✋ Nice to meet you",
+    "I'm a computer science student, a photographer, a musician, snowboarder and a gamer 🎮",
+    "I completed a 2 years program of computer technician which provided me a solid computer background",
+    "I love technology, I'm motivated to learn more and I dreamed of becoming an awesome software engineer one day",
+    "I started my programming journey as a self-taught. Then I fell in-love, so I decided to take a computer science program",
+    "I've built several projects and collaboration with other students",
+    "Fun fact about me, every year I aim to learn something new. For the year 2022, I'm trying my best to learn long-board",
+    "Oh! and I like to build customize keyboard.",
+    "If you're interested to know me better, feel free to contact. I'm thrilled to tell you my story."
+
+];
+
+whoami = [
+    "<br>",
+    '"who am i?" I don\'t know who you are but I\'m interested to hear your story.'
+];
+
+social = [
+    "<br>",
+    
+]
